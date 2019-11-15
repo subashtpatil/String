4f59ee9a9a6f65c518b399a2791d77e583e4539e
subashtpatil <56106616+subashtpatil@users.noreply.github.com>
2019-11-15 12:02:53 +0530

    this is nov 16th commit

---
+++ Demo1.java
@@ -0,0 +1,13 @@
+package String;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		String S1 = "Hello";
+		String S2 = new String("Bangalore");
+		System.out.println(S1+" "+S2);
+	}
+}
+
+//Output   Hello Bangalore
+
---
+++ Demo2.java
@@ -0,0 +1,41 @@
+package String;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		/*String S1="Java";
+		String S2= new String("Developer");
+		String S3="Java";
+		String S4= new String("Developer");
+		System.out.println(S1==S3);
+		System.out.println(S2==S4);
+		String S5= new String("Developer");
+		System.out.println(S1==S5);
+	}
+
+}
+true
+false
+false*/
+		
+		/*String S1="Java";
+		String S2= new String("Developer");
+		String S3="Java";
+		String S4= new String("Developer");
+		String S5= new String("Java");
+		System.out.println(S1==S3);  //true
+		System.out.println(S2==S4);  //false
+		System.out.println(S1==S5);  //false
+		System.out.println(S1.equals(S3));  //true
+		System.out.println(S2.equals(S4));  //true
+		System.out.println(S1.equals(S5));  //true
+		System.out.println(S1.equals(S2));  //false
+*/		
+		
+		String S2= new String("Developer");
+		String S4= new String("Developer");
+		System.out.println(S2);
+		System.out.println(S2==S4);
+		System.out.println(S2.equals(S4));
+		}
+		}
